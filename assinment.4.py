0dfde4f4d514f9983b485dd94798bfc35510af13
Muhammadpiaic <muhammadzaibpiaic@gmail.com>
2024-10-03 14:42:32 +0500

    Add files via upload

---
+++ assinment.4.py
@@ -0,0 +1,14 @@
+num1=5
+num2=6
+#subtract (+)
+result=num1+num2
+print(result)
+#subtract (-)
+result=num1-num2
+print(result)
+#subtract (*)
+result=num1*num2
+print(result)
+#subtract (/)
+result=num1/num2
+print(result)
